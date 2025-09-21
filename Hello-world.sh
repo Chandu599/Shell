3342709b3b0ef15932760853bc080ae7df5a14c7
Chandu Sai <kannalichandureddy@gmail.com>
2025-09-21 07:50:53 +0530

    Conversation between Suresh and Ramesh

--- Hello-world.sh
+++ Hello-world.sh
@@ -3,3 +3,8 @@
 # this is comment and will not execute
 
 echo "Hello World, I've started procaticing Shell scripting"
+
+echo "Suresh::Hi Ramesh, How are you"
+echo "Ramesh::Hi Suresh, I'm good. How are you?"
+echo "Suresh::I'm fine, what are you learning now"
+echo "Ramesh::I've started learning DevOps"
