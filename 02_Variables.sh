c92e96762e96675d6f6d01f12f1e4001bfeb41cd
Chandu Sai <kannalichandureddy@gmail.com>
2025-09-22 22:15:12 +0530

    Arithmetic calculations

--- 02_Variables.sh
+++ 02_Variables.sh
@@ -5,7 +5,7 @@ echo "Enter number:"
 read NUMBER
 
 if [ $(($number % 2)) -eq 0 ]; then
-    echo $NUMBER is even
+    echo "$NUMBER is even"
 else
-    echo $NUMBER is odd
+    echo "$NUMBER is odd"
 fi
